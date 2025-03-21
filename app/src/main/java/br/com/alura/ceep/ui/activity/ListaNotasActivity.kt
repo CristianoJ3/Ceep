12fb06b371954b716f05de91f1ab26c6d415bf5a
CristianoJ3 <cristianocarmo.junior@gmail.com>
2025-03-21 14:14:13 -0300

    Projeto Ceep realizado através do curso de Web API com Kotlin pela Alura.

---
+++ app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.kt
@@ -0,0 +1,85 @@
+package br.com.alura.ceep.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import br.com.alura.ceep.database.AppDatabase
+import br.com.alura.ceep.databinding.ActivityListaNotasBinding
+import br.com.alura.ceep.extensions.vaiPara
+import br.com.alura.ceep.model.Nota
+import br.com.alura.ceep.ui.recyclerview.adapter.ListaNotasAdapter
+import br.com.alura.ceep.webclient.RetrofitInicializador
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.Retrofit
+
+class ListaNotasActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityListaNotasBinding.inflate(layoutInflater)
+    }
+    private val adapter by lazy {
+        ListaNotasAdapter(this)
+    }
+    private val dao by lazy {
+        AppDatabase.instancia(this).notaDao()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        configuraFab()
+        configuraRecyclerView()
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                buscaNotas()
+            }
+        }
+
+        val call: Call<List<Nota>> = RetrofitInicializador().notaService.buscaTodas()
+        // chama a requisição síncrona da call através do execute()
+        val resposta: Response<List<Nota>> = call.execute()
+        resposta.body()?.let { notas ->
+            Log.i("ListaNotas", "onCreate: $notas")
+        }
+    }
+
+    private fun configuraFab() {
+        binding.activityListaNotasFab.setOnClickListener {
+            Intent(this, FormNotaActivity::class.java).apply {
+                startActivity(this)
+            }
+        }
+    }
+
+    private fun configuraRecyclerView() {
+        binding.activityListaNotasRecyclerview.adapter = adapter
+        adapter.quandoClicaNoItem = { nota ->
+            vaiPara(FormNotaActivity::class.java) {
+                putExtra(NOTA_ID, nota.id)
+            }
+        }
+    }
+
+    private suspend fun buscaNotas() {
+        dao.buscaTodas()
+            .collect { notasEncontradas ->
+                binding.activityListaNotasMensagemSemNotas.visibility =
+                    if (notasEncontradas.isEmpty()) {
+                        binding.activityListaNotasRecyclerview.visibility = GONE
+                        VISIBLE
+                    } else {
+                        binding.activityListaNotasRecyclerview.visibility = VISIBLE
+                        adapter.atualiza(notasEncontradas)
+                        GONE
+                    }
+            }
+    }
+}
